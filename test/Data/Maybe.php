bbc0e61553d558486c2d21598623a9e0e9ca342a
katsujukou <katsujukou@gmail.com>
2021-04-05 01:59:42 +0900

    implement CLI phalg

---
+++ test/Data/Maybe.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Test\Data\Maybe;
+
+
+/** @template t */
+abstract class Maybe extends \SimpleADT\ADT
+{
+    /**
+     * @adt-constructor
+     * @return Maybe<mixed>
+     */
+    public static function Nothing() :Maybe {
+        return self::create();
+    }
+
+    /**
+     * @adt-constructor
+     * @template T
+     * @param T $val
+     * @return Maybe<T>
+     */
+    public static function Just($val) :Maybe {
+        return self::create($val);
+    }
+
+}
